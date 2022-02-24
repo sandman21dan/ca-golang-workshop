367ec4cc01148828bafd3cd6f9925b2b319e4e94
Daniel Perez <daniel.perez@complyadvantage.com>
2022-02-24 17:45:26 +0000

    add password generator solution

--- app/pkg/track1/passwordgenerator/password_generator.go
+++ app/pkg/track1/passwordgenerator/password_generator.go
@@ -1,5 +1,11 @@
 package passwordgenerator
 
+import (
+	"errors"
+	"math/rand"
+	"unicode"
+)
+
 type GeneratePasswordOptions struct {
 	Digits            bool
 	Letters           bool
@@ -7,7 +13,53 @@ type GeneratePasswordOptions struct {
 	SpecialCharacters bool
 }
 
+var digits = []rune("0123456789")
+var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+var special = []rune("$%&*#@")
+
+const (
+	digit = iota
+	letter
+	uppercase
+	specialCharacter
+)
+
 func GeneratePassword(length int, opt GeneratePasswordOptions) (string, error) {
-	// Your code here
-	return "", nil
+	if !(opt.Digits || opt.Letters || opt.SpecialCharacters) {
+		return "", errors.New("invalid set of arguments, at least one must be true")
+	}
+
+	password := []rune{}
+
+	optPool := []int{}
+	if opt.Digits {
+		optPool = append(optPool, digit)
+	}
+	if opt.Letters {
+		optPool = append(optPool, letter)
+	}
+	if opt.SpecialCharacters {
+		optPool = append(optPool, specialCharacter)
+	}
+	if opt.Uppercase {
+		optPool = append(optPool, uppercase)
+	}
+
+	for i := 0; i < length; i++ {
+		randomOpt := optPool[rand.Intn(len(optPool))]
+
+		switch randomOpt {
+		case digit:
+			password = append(password, digits[rand.Intn(len(digits))])
+		case letter:
+			password = append(password, letters[rand.Intn(len(letters))])
+		case specialCharacter:
+			password = append(password, special[rand.Intn(len(special))])
+		case uppercase:
+			letter := letters[rand.Intn(len(letters))]
+			password = append(password, unicode.ToUpper(letter))
+		}
+	}
+
+	return string(password), nil
 }
