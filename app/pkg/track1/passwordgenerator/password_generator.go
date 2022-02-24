05c12162dce82529e6300be42270a77e3b93ef37
Daniel Perez <daniel.perez@complyadvantage.com>
2022-02-24 17:22:49 +0000

    remove code pushed by mistake

--- app/pkg/track1/passwordgenerator/password_generator.go
+++ app/pkg/track1/passwordgenerator/password_generator.go
@@ -1,7 +1,5 @@
 package passwordgenerator
 
-import "errors"
-
 type GeneratePasswordOptions struct {
 	Digits            bool
 	Letters           bool
@@ -9,23 +7,7 @@ type GeneratePasswordOptions struct {
 	SpecialCharacters bool
 }
 
-var timesCount = 0
-
 func GeneratePassword(length int, opt GeneratePasswordOptions) (string, error) {
 	// Your code here
-	// return "abcdesgj", nil
-
-	if !(opt.Digits || opt.Letters || opt.Uppercase || opt.SpecialCharacters) {
-		return "", errors.New("invalid set of arguments, at least one must be true")
-	}
-
-	answers := []string{
-		"123456",
-		"abcdefhi",
-		"aBcDeFgHiJlk",
-		"aB2DeFg$iJ",
-	}
-
-	timesCount++
-	return answers[timesCount-1], nil
+	return "", nil
 }
